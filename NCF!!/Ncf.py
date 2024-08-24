a8532d6abd39fdb5745cac2eeaad7c5ed7f8c24f
NCF-KFC <nathalie.chacana20@gmail.com>
2024-08-24 15:12:05 +0200

    poo

--- NCF!!/Ncf.py
+++ NCF!!/Ncf.py
@@ -1,13 +1,16 @@
-
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 
-class ncfholdApp(App):
+class Cybergottalent(App):
     def go_to_screen(self, screenName):
         self.root.current = screenName
-        
-ncfholdApp().run()
+
+    def build(self):
+        return super().build()
+
+Cybergottalent().run()
